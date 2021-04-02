3a1553bc0874d9e0051c5ad5f57daf65f010ae94
carolZapata <carozapatac@gmail.com>
2021-04-02 02:37:05 +0200

    [ADD] Proyecto spring boot configurado

---
+++ proyectoFinalSpring/src/main/java/com/carol/checkpost/ProyectoFinalSpringApplication.java
@@ -0,0 +1,13 @@
+package com.carol.checkpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalSpringApplication.class, args);
+	}
+
+}
---
+++ proyectoFinalSpring/src/test/java/com/carol/demo/ProyectoFinalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carol.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
