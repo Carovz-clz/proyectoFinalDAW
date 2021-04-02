3cc3daae3e0484be381b193b4b51d417b55d0bc6
carolZapata <carozapatac@gmail.com>
2021-04-02 14:59:46 +0200

    [ADD] Componente login con dormulario. Rutas de inicio y alta añadidas.

--- proyectoFinalAngular/src/app/app-routing.module.ts
+++ proyectoFinalAngular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AltaUsuarioComponent } from './alta-usuario/alta-usuario.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: LoginComponent, pathMatch: 'full'},
+  {path: 'alta', component: AltaUsuarioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proyectoFinalAngular/src/app/app.module.ts
+++ proyectoFinalAngular/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AltaUsuarioComponent } from './alta-usuario/alta-usuario.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AltaUsuarioComponent
+    AltaUsuarioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
