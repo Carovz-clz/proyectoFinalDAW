8fb91ec1708a9f696cd859087428230ce3fef2b4
carolZapata <carozapatac@gmail.com>
2021-05-18 23:23:39 +0200

    [FIX] Cambio en el script de la bbdd para correcto funcionamiento al crear imagen docker

--- scriptSql/bbddProyectoFinal.sql
+++ scriptSql/bbddProyectoFinal.sql
@@ -1,77 +1,172 @@
-drop database if exists proyectofinal;
-create database proyectofinal;
-use proyectofinal;
+CREATE DATABASE  IF NOT EXISTS `proyectofinal` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `proyectofinal`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: proyectofinal
+-- ------------------------------------------------------
+-- Server version	8.0.22
 
-CREATE TABLE `proyectofinal`.`users` (
-  `username` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(50) NOT NULL,
-  `nombre` VARCHAR(45) NULL,
-  `apellidos` VARCHAR(45) NULL,
-  `correo_electronico` VARCHAR(45) NULL,
-  `visible` TINYINT(1) NOT NULL DEFAULT 1,
-  `enabled` TINYINT(1) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`username`))
-COMMENT = 'Tabla usuarios';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `proyecto`
+--
 
-CREATE TABLE `proyectofinal`.`tipoUsuario` (
-  `id` INT NOT NULL,
-  `tipo` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-COMMENT = 'Tipos de usuarios';
+DROP TABLE IF EXISTS `proyecto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `proyecto` (
+  `idproyecto` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `fecha` date NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  PRIMARY KEY (`idproyecto`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Tabla de proyectos';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-    
-CREATE TABLE `proyectofinal`.`proyecto` (
-  `idproyecto` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `fecha` DATE NOT NULL,
-  `descripcion` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`idproyecto`))
-COMMENT = 'Tabla de proyectos';
+--
+-- Dumping data for table `proyecto`
+--
 
-CREATE TABLE `proyectofinal`.`usuarioProyecto` (
-  `idusuario_proyecto` INT NOT NULL AUTO_INCREMENT,
-  `id_proyecto` INT NOT NULL,
-  `usuario` VARCHAR(50) NOT NULL,
-  `tipo_usuario` INT NOT NULL,
-  PRIMARY KEY (`idusuario_proyecto`),
-  CONSTRAINT `id_proyecto`
-    FOREIGN KEY (`id_proyecto`)
-    REFERENCES `proyectofinal`.`proyecto` (`idproyecto`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `usuario`
-    FOREIGN KEY (`usuario`)
-    REFERENCES `proyectofinal`.`users` (`username`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `tipo_usuario`
-    FOREIGN KEY (`tipo_usuario`)
-    REFERENCES `proyectofinal`.`tipoUsuario` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-COMMENT = 'Tabla que recoge el tipo de usuario de cada proyecto';
+LOCK TABLES `proyecto` WRITE;
+/*!40000 ALTER TABLE `proyecto` DISABLE KEYS */;
+INSERT INTO `proyecto` VALUES (1,'Proyecto final','2021-05-17','Proyecto final de curso, página web');
+/*!40000 ALTER TABLE `proyecto` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE `proyectofinal`.`tarea` (
-  `idtarea` INT NOT NULL AUTO_INCREMENT,
-  `idproyecto` INT NOT NULL,
-  `descripcion` VARCHAR(100) NOT NULL,
-  `realizada` TINYINT(1) NOT NULL DEFAULT 0,
+--
+-- Table structure for table `tarea`
+--
+
+DROP TABLE IF EXISTS `tarea`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tarea` (
+  `idtarea` int NOT NULL AUTO_INCREMENT,
+  `idproyecto` int NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `realizada` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`idtarea`),
-  CONSTRAINT `idproyecto`
-    FOREIGN KEY (`idproyecto`)
-    REFERENCES `proyectofinal`.`proyecto` (`idproyecto`)
-	ON DELETE CASCADE
-	ON UPDATE CASCADE);
-    
-    
-SET GLOBAL time_zone = '+1:00';
+  KEY `idproyecto` (`idproyecto`),
+  CONSTRAINT `idproyecto` FOREIGN KEY (`idproyecto`) REFERENCES `proyecto` (`idproyecto`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-insert into tipoUsuario values (1, 'creador');
-insert into tipoUsuario values (2, 'colaborador');
-    
+--
+-- Dumping data for table `tarea`
+--
 
+LOCK TABLES `tarea` WRITE;
+/*!40000 ALTER TABLE `tarea` DISABLE KEYS */;
+INSERT INTO `tarea` VALUES (1,1,'Configurar estilos',0),(2,1,'Generar imágenes',1),(3,1,'Borrar console logs',0);
+/*!40000 ALTER TABLE `tarea` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `tipousuario`
+--
 
+DROP TABLE IF EXISTS `tipousuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tipousuario` (
+  `id` int NOT NULL,
+  `tipo` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Tipos de usuarios';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `tipousuario`
+--
 
+LOCK TABLES `tipousuario` WRITE;
+/*!40000 ALTER TABLE `tipousuario` DISABLE KEYS */;
+INSERT INTO `tipousuario` VALUES (1,'creador'),(2,'colaborador');
+/*!40000 ALTER TABLE `tipousuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellidos` varchar(45) DEFAULT NULL,
+  `correo_electronico` varchar(45) DEFAULT NULL,
+  `visible` tinyint(1) NOT NULL DEFAULT '1',
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Tabla usuarios';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('negro','pass','Carlos','Zapata','negro@gmail.ve',1,1),('pedropo','pass','Pedro','Po','pedro@calasanz.es',0,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarioproyecto`
+--
+
+DROP TABLE IF EXISTS `usuarioproyecto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarioproyecto` (
+  `idusuario_proyecto` int NOT NULL AUTO_INCREMENT,
+  `id_proyecto` int NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `tipo_usuario` int NOT NULL,
+  PRIMARY KEY (`idusuario_proyecto`),
+  KEY `id_proyecto` (`id_proyecto`),
+  KEY `usuario` (`usuario`),
+  KEY `tipo_usuario` (`tipo_usuario`),
+  CONSTRAINT `id_proyecto` FOREIGN KEY (`id_proyecto`) REFERENCES `proyecto` (`idproyecto`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tipo_usuario` FOREIGN KEY (`tipo_usuario`) REFERENCES `tipousuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `usuario` FOREIGN KEY (`usuario`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Tabla que recoge el tipo de usuario de cada proyecto';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarioproyecto`
+--
+
+LOCK TABLES `usuarioproyecto` WRITE;
+/*!40000 ALTER TABLE `usuarioproyecto` DISABLE KEYS */;
+INSERT INTO `usuarioproyecto` VALUES (1,1,'negro',1);
+/*!40000 ALTER TABLE `usuarioproyecto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'proyectofinal'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-18 22:40:01
